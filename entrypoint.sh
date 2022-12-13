ccc1227407047e262cc176baaee421e1ecde0498
Nadir Hamid <matrix.nad@gmail.com>
2022-12-13 13:56:18 -0700

    first

---
+++ entrypoint.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+set -e
+
+: ${CLOUD=""} # One of aws, azure, do, gcp, or empty
+if [ "$CLOUD" != "" ]; then
+   PROVIDER="-provider ${CLOUD}"
+fi
+
+PRIVATE_IPV4=$(netdiscover -field privatev4 ${PROVIDER})
+PUBLIC_IPV4=$(netdiscover -field publicv4 ${PROVIDER})
+PROXY_IPV4=${PUBLIC_IPV4}
+
+
+for filename in /etc/asterisk/*.conf; do
+    CFG_PATH="${filename}"
+    sed "s/PRIVATE_IPV4/${PRIVATE_IPV4}/g" $CFG_PATH > $CFG_PATH.cop
+    sed "s/PUBLIC_IPV4/${PUBLIC_IPV4}/g" $CFG_PATH.cop > $CFG_PATH.cop2
+    sed "s/PROXY_IPV4/${PROXY_IPV4}/g" $CFG_PATH.cop2 > $CFG_PATH.cop3
+    sed "s/LINEBLOCS_KEY/${LINEBLOCS_KEY}/g" $CFG_PATH.cop3 > $CFG_PATH.final
+    rm -rf $CFG_PATH.cop*
+    yes|mv  $CFG_PATH.final $CFG_PATH
+done
+
+
+# Default values
+ASTERISK_ARGS="-fp"
+
+# Run Asterisk
+/usr/sbin/asterisk ${ASTERISK_ARGS}
