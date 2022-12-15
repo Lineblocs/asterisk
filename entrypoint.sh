171693f7efe0d878ca8cb80632828302bb648048
Nadir Hamid <matrix.nad@gmail.com>
2022-12-15 13:39:58 -0700

    add GENERATED_ARI_PASSWORD

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,6 +9,7 @@ fi
 PRIVATE_IPV4=$(netdiscover -field privatev4 ${PROVIDER})
 PUBLIC_IPV4=$(netdiscover -field publicv4 ${PROVIDER})
 PROXY_IPV4=${PUBLIC_IPV4}
+GENERATED_ARI_PASSWORD=${GENERATED_ARI_PASSWORD}
 
 
 for filename in /etc/asterisk/*.conf; do
@@ -16,7 +17,8 @@ for filename in /etc/asterisk/*.conf; do
     sed "s/PRIVATE_IPV4/${PRIVATE_IPV4}/g" $CFG_PATH > $CFG_PATH.cop
     sed "s/PUBLIC_IPV4/${PUBLIC_IPV4}/g" $CFG_PATH.cop > $CFG_PATH.cop2
     sed "s/PROXY_IPV4/${PROXY_IPV4}/g" $CFG_PATH.cop2 > $CFG_PATH.cop3
-    sed "s/LINEBLOCS_KEY/${LINEBLOCS_KEY}/g" $CFG_PATH.cop3 > $CFG_PATH.final
+    sed "s/LINEBLOCS_KEY/${LINEBLOCS_KEY}/g" $CFG_PATH.cop3 > $CFG_PATH.cop4
+    sed "s/GENERATED_ARI_PASSWORD/${GENERATED_ARI_PASSWORD}/g" $CFG_PATH.cop4 > $CFG_PATH.final
     rm -rf $CFG_PATH.cop*
     yes|mv  $CFG_PATH.final $CFG_PATH
 done
