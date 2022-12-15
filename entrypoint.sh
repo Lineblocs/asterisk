26d92f7559d26b2f22941c5c5797d02d9e21784a
Nadir Hamid <matrix.nad@gmail.com>
2022-12-15 13:42:03 -0700

    make small modifications for ARI

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,7 +9,6 @@ fi
 PRIVATE_IPV4=$(netdiscover -field privatev4 ${PROVIDER})
 PUBLIC_IPV4=$(netdiscover -field publicv4 ${PROVIDER})
 PROXY_IPV4=${PUBLIC_IPV4}
-GENERATED_ARI_PASSWORD=${GENERATED_ARI_PASSWORD}
 
 
 for filename in /etc/asterisk/*.conf; do
@@ -18,7 +17,7 @@ for filename in /etc/asterisk/*.conf; do
     sed "s/PUBLIC_IPV4/${PUBLIC_IPV4}/g" $CFG_PATH.cop > $CFG_PATH.cop2
     sed "s/PROXY_IPV4/${PROXY_IPV4}/g" $CFG_PATH.cop2 > $CFG_PATH.cop3
     sed "s/LINEBLOCS_KEY/${LINEBLOCS_KEY}/g" $CFG_PATH.cop3 > $CFG_PATH.cop4
-    sed "s/GENERATED_ARI_PASSWORD/${GENERATED_ARI_PASSWORD}/g" $CFG_PATH.cop4 > $CFG_PATH.final
+    sed "s/ARI_PASSWORD/${ARI_PASSWORD}/g" $CFG_PATH.cop4 > $CFG_PATH.final
     rm -rf $CFG_PATH.cop*
     yes|mv  $CFG_PATH.final $CFG_PATH
 done
