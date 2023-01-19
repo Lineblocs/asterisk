4c92420c52284bdb4f147b77d0071e1d87fce000
Nadir Hamid <matrix.nad@gmail.com>
2023-01-19 16:39:27 -0700

    allow AMI_PASS to be updated

--- entrypoint.sh
+++ entrypoint.sh
@@ -17,7 +17,8 @@ for filename in /etc/asterisk/*.conf; do
     sed "s/PUBLIC_IPV4/${PUBLIC_IPV4}/g" $CFG_PATH.cop > $CFG_PATH.cop2
     sed "s/PROXY_IPV4/${PROXY_IPV4}/g" $CFG_PATH.cop2 > $CFG_PATH.cop3
     sed "s/LINEBLOCS_KEY/${LINEBLOCS_KEY}/g" $CFG_PATH.cop3 > $CFG_PATH.cop4
-    sed "s/ARI_PASSWORD/${ARI_PASSWORD}/g" $CFG_PATH.cop4 > $CFG_PATH.final
+    sed "s/ARI_PASSWORD/${ARI_PASSWORD}/g" $CFG_PATH.cop4 > $CFG_PATH.cop5
+    sed "s/AMI_PASS/${AMI_PASS}/g" $CFG_PATH.cop5 > $CFG_PATH.final
     rm -rf $CFG_PATH.cop*
     yes|mv  $CFG_PATH.final $CFG_PATH
 done
